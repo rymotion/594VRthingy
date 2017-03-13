9cbfb3833ec8137b8096c5a77c39431447ee2448
rymotion <rymotion1@me.com>
2017-03-13 14:22:52 -0700

    adding kasey's files

---
+++ SW_VR/Assets/Assets/Scripts/Start.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+	void OnGUI(){
+		if (GUI.Button (new Rect (0, 0, 200, 200), "Quit")) {
+			// get to activity
+			var androidJC = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			var jo = androidJC.GetStatic<AndroidJavaObject>("currentActivity");
+
+			//access the class for Android
+			var jc = new AndroidJavaClass("com.example.fboomplug.StartActivity");
+			// paging Android activity paging android activity
+			jc.CallStatic("Call" , jo);
+		}
+	}
+}
--- app/src/main/java/com/example/ryanpaglinawan/a594vrthingy/MainActivity.java
+++ app/src/main/java/com/example/ryanpaglinawan/a594vrthingy/MainActivity.java
@@ -1,29 +1,28 @@
 package com.example.ryanpaglinawan.a594vrthingy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+        private String TAG = "Plug.StartActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Activity created");
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    public static void Call(Activity activity){
+        //  Start Unity Activity
+        Intent myIntent = new Intent(activity, MainActivity.class);
+        activity.startActivity(myIntent);
+    }
+    public static void getScore(Int score){
+        // Should modify text field to be the score from the game
     }
 }
+
